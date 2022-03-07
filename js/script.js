3c638e43438beee17e981470caeb43291bf94f1b
Linnea-BergquistOlsson-Nti-Johanneberg <94617296+Linnea-BergquistOlsson-Nti-Johanneberg@users.noreply.github.com>
2022-03-07 10:12:22 +0100

    Lektion 07/03/22

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // Denna fil skall innehålla JavaScript (JS)
 
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
+    console.log("HEJ");
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
